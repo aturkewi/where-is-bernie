bf13e5b122691c2209ff5d94f822096e95d2151d
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-26 16:08:24 -0500

    Add models

---
+++ db/migrate/20160126210625_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :url
+      t.datetime :start
+      t.text :description
+      t.string :event_type
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20160126210812_create_locations.rb
@@ -0,0 +1,15 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.float :lat
+      t.float :long
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+
+      t.timestamps null: false
+    end
+  end
+end
